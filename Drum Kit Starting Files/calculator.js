5c2ba025c7b851c244b650f16b4192c57d6e0631
ApesZhao <83993205+ApesZhao@users.noreply.github.com>
2022-06-08 21:20:17 +1000

    Update calculator.js

--- Drum Kit Starting Files/calculator.js
+++ Drum Kit Starting Files/calculator.js
@@ -25,8 +25,47 @@ function calculator(num1,num2,operator){
 	return operator(num1,num2);
 }
 
+// 8/06/2022
+//Constructor Function (function name needs to be capitalised)
+
+function BellBoy(name,age,hasWorkPermit,language){
+	this.name=name;
+	this.age=age;
+	this.hasWorkPermit=hasWorkPermit;
+	this.language=language;
+	
+	//including a function
+	this.moveSuitcase=function(){
+		alert("May I take your suitcase?");
+		pickUpSuitcase();
+		move();
+		
+	}
+	
+}
+
+//Initialise Objects:
+var bellBoy1 = new BellBoy("Timmy",19,true,["Chinese","English"]);
+
+//using the function in constructor function
+//bellBoy1.moveSuitcase();
 
 
 
 
+//Creating a housekeeper constructor function
 
+function HouseKeeper(name, yearsOfExp, cleaningRepertoire){
+	
+	this.name=name;
+	this.yearsOfExp=yearsOfExp;
+	this.cleaningRepertoire=cleaningRepertoire;
+	this.cleaning=function(){
+		alert("Cleaning starts");
+		
+	}
+}
+//creating a new housekeeper obj
+var housekeeper1=new HouseKeeper("Fiona", 5, ["Rooms","dirty rooms"]);
+///console.log(housekeeper1.name);
+//housekeeper1.cleaning();
