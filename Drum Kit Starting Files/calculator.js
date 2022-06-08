009fd83df3cc6973ed86d17e65d6fcce92e79c36
ApesZhao <83993205+ApesZhao@users.noreply.github.com>
2022-06-08 17:02:35 +1000

    updates for 8/06/2022
    updates for 8/06/2022

---
+++ Drum Kit Starting Files/calculator.js
@@ -0,0 +1,32 @@
+
+
+// Calculator function, function inside function
+
+
+function add(num1,num2){
+
+	return num1+num2;
+}
+function substract(num1,num2){
+	
+	return num1-num2;
+}
+function mulitply(num1,num2){
+	
+	return num1*num2;
+}
+
+function divide(num1,num2){
+	
+	return num1/num2;
+}
+
+function calculator(num1,num2,operator){	
+	return operator(num1,num2);
+}
+
+
+
+
+
+
--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -10,6 +10,49 @@ var audiok = new Audio('sounds/crash.mp3');
 var audiol = new Audio('sounds/kick-bass.mp3');
 
 
+//key down event added 8/06/2022
+//detecting key pressed
+document.addEventListener("keydown",respondToKey);
+function respondToKey(event) {
+
+		switch(event.key){
+		case "w":
+		audiow.play();
+		break;
+		
+		case "a":
+		audioa.play();
+		break;
+				
+		case "s":
+		audios.play();
+		break;
+		
+		case "d":
+		audiod.play();
+		break;
+		
+		case "j":
+		audioj.play();
+		break;
+		
+		case "k":
+		audiok.play();
+		break;
+		
+		case "l":
+		audiol.play();
+		break;
+		
+		default:
+		alert("Only press the buttons on screen!");
+		
+	}
+	
+		buttonAnimation(event.key);
+}
+
+//detecting window button
 for(n=0;n<=document.querySelectorAll(".drum").length;n++){
 	document.querySelectorAll(".drum")[n].addEventListener("click",buttonClick);
 }
@@ -98,8 +141,15 @@ function buttonClick(){
 		alert(this.innerHTML);
 		
 	}
+	buttonAnimation(this.innerHTML);
 	
-	
+}
+
+function buttonAnimation(currentKey) {
+
+    //the button we want to change the style
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
 }
 
 
@@ -113,10 +163,23 @@ function BellBoy(name,age,hasWorkPermit,language){
 	this.hasWorkPermit=hasWorkPermit;
 	this.language=language;
 	
+	//including a function
+	this.moveSuitcase=function(){
+		alert("May I take your suitcase?");
+		pickUpSuitcase();
+		move();
+		
+	}
+	
 }
 
 //Initialise Objects:
 var bellBoy1 = new BellBoy("Timmy",19,true,["Chinese","English"]);
+
+//using the function in constructor function
+//bellBoy1.moveSuitcase();
+
+
 
 
 //Creating a housekeeper constructor function
@@ -126,35 +189,13 @@ function HouseKeeper(name, yearsOfExp, cleaningRepertoire){
 	this.name=name;
 	this.yearsOfExp=yearsOfExp;
 	this.cleaningRepertoire=cleaningRepertoire;
+	this.cleaning=function(){
+		alert("Cleaning starts");
+		
+	}
 }
 //creating a new housekeeper obj
 var housekeeper1=new HouseKeeper("Fiona", 5, ["Rooms","dirty rooms"]);
-console.log(housekeeper1.name);
+///console.log(housekeeper1.name);
+//housekeeper1.cleaning();
 
-// Calculator function, function inside function
-
-
-function add(num1,num2){
-
-	return num1+num2;
-}
-function substract(num1,num2){
-	
-	return num1-num2;
-}
-function mulitply(num1,num2){
-	
-	return num1*num2;
-}
-
-function divide(num1,num2){
-	
-	return num1/num2;
-}
-
-function calculator(num1,num2,operator){
-	
-	return operator(num1,num2);
-}
-;
-}
